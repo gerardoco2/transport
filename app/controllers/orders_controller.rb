2f301f6813c032b7dd52eac12e228dc402b92cdb
Gerardo Colina <gerardocolina@gmail.com>
2018-06-02 21:38:57 -0400

    form's grid modified and set status in the controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,6 +28,7 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.reservation = Time.new.strftime("%H%M%m%d%Y")
     @order.status = "Pending / Contract sent"
+    @order.balance_on_delivery = @order.driver_pay
     tax = Float(@order.tax/100) 
     @order.grand_total = @order.booking_deposit + @order.driver_pay * (1 + tax)
  
