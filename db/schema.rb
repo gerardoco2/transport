b7d2fd88ab8b9b24bae4fa43819d53c0e0f2ec3c
Gerardo Colina <gerardocolina@gmail.com>
2018-06-07 03:57:41 -0400

    se implemento la gem devise y se agrego el atributo name en su migracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603010057) do
+ActiveRecord::Schema.define(version: 20180605211812) do
 
   create_table "cities", force: :cascade do |t|
     t.integer "province_id"
@@ -116,6 +116,24 @@ ActiveRecord::Schema.define(version: 20180603010057) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
